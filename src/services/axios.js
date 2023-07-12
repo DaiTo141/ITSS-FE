c05a7cc2cde7c81580756efdbc843225104b0b5c
huyhoan1109 <huyhoan0901@gmail.com>
2023-07-12 18:36:16 +0700

    update header

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,7 +2,9 @@ import axios from "axios";
 const Api = axios.create({
     baseURL: process.env.REACT_APP_BE_URL,
     headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        // 'Access-Control-Allow-Origin' : true,
+        // 'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH'
     },
 });
 export default Api
